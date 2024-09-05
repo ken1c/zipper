f401b761acf6a282c853b1a393ef7492c34ca892
ken1c <mhemansakkthivel@gmail.com>
2024-09-05 12:57:21 +0530

    min-heap insertion

--- src/main/java/heman/App.java
+++ src/main/java/heman/App.java
@@ -1,6 +1,51 @@
 package heman;
 
+class minHeap {
+    int[] arr;
+    int i,n, length,currIndex,parIndex;
+    minHeap(int length) {
+        i=0;
+        arr = new int[length];
+    }
+
+    public void Insertion(int num) {    
+        if (i< arr.length) {
+
+            arr[i]=num;
+            currIndex = i;
+            i++;
+
+            while (currIndex > 0) {
+                parIndex=(currIndex-1)/2;
+               if (arr[currIndex] < arr[parIndex]) {
+                    n = arr[currIndex];
+                    arr[currIndex] = arr[parIndex];
+                    arr[parIndex] = n;
+
+                    currIndex = parIndex;
+               } else {
+                break;
+               }
+            }
+        }
+    }
+
+    public void PrintHeap() {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
 public class App 
 {
-    //I need to implement heap ds 😭
+    public static void main(String[] args) {
+        minHeap m = new minHeap(5);
+        m.Insertion(6);
+        m.Insertion(4);
+        m.Insertion(7);
+        m.Insertion(2);
+        m.Insertion(1);
+        m.PrintHeap();
+    }
 }
